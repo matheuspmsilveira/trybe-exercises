497aa76a2071cd8d5da3412ed247ec3d7ac8e939
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-06-23 18:18:57 -0300

    Exercício 3: Adiciona um programa que retorne o maior de três números, definidos previamente em variáveis.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercise-3.js
@@ -0,0 +1,11 @@
+let num1 = 3;
+let num2 = 13;
+let num3 = 23;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log(num2);
+} else {
+  console.log(num3);
+}
