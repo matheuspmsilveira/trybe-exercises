1ee5fbf793e9a4b7514a8d97a00f444b19b98911
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-06-24 16:22:14 -0300

    Modifica exercício 5 para deixar melhor a variavel highNumber Adiciona Exercício 7: Que descubre qual o menor valor contido no array e o imprime

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercise-5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercise-5.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let highNumber = 0;
+let highNumber = numbers[0];
 
 for (index = 0; index < numbers.length; index += 1) {
   if (numbers[index] > highNumber) {
