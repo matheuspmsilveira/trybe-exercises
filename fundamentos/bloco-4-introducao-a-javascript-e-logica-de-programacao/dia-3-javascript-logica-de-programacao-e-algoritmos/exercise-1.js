3e9c8915c6efaf8494b1a19850729dfa4d0e9832
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-06-25 16:42:22 -0300

    Exercicio 1: ara o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise-1.js
@@ -0,0 +1,12 @@
+let n = 5;
+let asterisco = "*";
+let numberAsteriscos = "";
+
+
+for (let index = 0; index < n; index += 1) {
+  numberAsteriscos += asterisco
+}
+
+for (let index = 0; index < n; index += 1) {
+  console.log(numberAsteriscos);
+}
