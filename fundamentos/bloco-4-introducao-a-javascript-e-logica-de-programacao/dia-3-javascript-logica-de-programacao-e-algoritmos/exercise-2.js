b537214c465f85399cef8421c390d1d8ee26d256
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-06-25 18:24:27 -0300

    exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise-2.js
@@ -1,8 +1,8 @@
 let n = 5;
 let asterisco = "*";
-let linhaAdicionarAsterisco = "";
+let inputPlace = "";
 
 for (let index = 0; index < n; index += 1) {
-  linhaAdicionarAsterisco += asterisco;
-  console.log(linhaAdicionarAsterisco);
+  inputPlace += asterisco;
+  console.log(inputPlace);
 }
