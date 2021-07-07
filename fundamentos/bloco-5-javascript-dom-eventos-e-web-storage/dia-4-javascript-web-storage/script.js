50750924e25d99a27d2818134f1712e8d521a7d8
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-07-07 20:01:46 -0300

    Faz com que os inputs mudem caracteriscas da pagina

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,10 +1,42 @@
-const inputBackground = document.getElementById('background-color-input');
-const buttonBackground = document.getElementById('background-button');
-const inputColor = document.getElementById('font-color-input');
-const buttonColor = document.getElementById('color-button');
-const inputFontSize = document.getElementById('font-size-input');
-const buttonFontSize = document.getElementById('font-size-button');
-const inputLineSpace = document.getElementById('line-space-input');
-const buttonLineSpace = document.getElementById('line-space-button');
-const inputFontFamily = document.getElementById('font-family-input');
-const buttonFontFamily = document.getElementById('font-family-button');
+window.onload = function () {
+  const inputBackground = document.getElementById('background-color-input');
+  const inputColor = document.getElementById('font-color-input');
+  const inputFontSize = document.getElementById('font-size-input');
+  const inputLineSpace = document.getElementById('line-space-input');
+  const inputFontFamily = document.getElementById('font-family-input');
+  
+  function changeBackgroundColor() {
+    inputBackground.addEventListener('input', function(event) {
+      document.body.style.backgroundColor = event.target.value;
+    })
+  }
+  changeBackgroundColor();
+
+  function changeFontColor() {
+    inputColor.addEventListener('input', function(event) {
+      document.body.style.color = event.target.value;
+    })
+  }
+  changeFontColor();
+
+  function changeFontSize() {
+    inputFontSize.addEventListener('input', function(event) {
+      document.querySelector('.news').style.fontSize = event.target.value;
+    })
+  }
+  changeFontSize();
+
+  function changeLineSpace() {
+    inputLineSpace.addEventListener('input', function(event) {
+      document.querySelector('.news').style.lineHeight = event.target.value;
+    })
+  }
+  changeLineSpace();
+
+  function changeFontFamily() {
+    inputFontFamily.addEventListener('input', function(event) {
+      document.body.style.lineHeight = event.target.value;
+    })
+  }
+  changeFontFamily();
+}
