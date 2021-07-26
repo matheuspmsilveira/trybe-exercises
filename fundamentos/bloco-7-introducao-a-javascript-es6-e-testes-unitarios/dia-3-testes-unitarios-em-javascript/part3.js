4f665f0ea0f39636df0fb082203d47b0f7069c3d
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-07-26 19:13:08 -0300

    Exercicio 3.2 - Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/part3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/part3.js
@@ -1,3 +1,4 @@
+//Exercicio 3.1
 const assert = require('assert');
 
 const greetPeople = (people) => {
@@ -13,3 +14,32 @@ const parameter = ['Irina', 'Ashleigh', 'Elsa'];
 const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 
 assert.deepStrictEqual(greetPeople(parameter), result);
+
+//Exercicio 3.2
+const removeVowels = (word) => {
+  const characters = word.split('');
+  let results = '';
+  let numberOfVogal = 0;
+
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      numberOfVogal += 1;
+      results += numberOfVogal;
+    } else {
+      results += characters[index];
+    }
+  }
+  return results;
+};
+
+
+const parameter1 = 'Dayane';
+const result1 = 'D1y2n3';
+
+assert.strictEqual(removeVowels(parameter1), result1);
