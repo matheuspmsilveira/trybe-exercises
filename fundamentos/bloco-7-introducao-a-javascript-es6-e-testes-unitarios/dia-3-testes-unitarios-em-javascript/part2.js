bc838c352edcb6c40cde7d23fe5002094769d360
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-07-26 18:37:43 -0300

    Exercicio 2.2 - Escreva a função wordLengths para passar nos testes já implementados.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/part2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/part2.js
@@ -2,9 +2,11 @@
 const assert = require('assert');
 const addOne = (array) => {
   let result = [];
+
   for (let index = 0; index < array.length; index += 1) {
     result.push(array[index] + 1);
   }
+
   return result;
 };
 
@@ -16,3 +18,22 @@ const output = addOne(myArray);
 assert.strictEqual(typeof addOne, 'function');
 assert.deepStrictEqual(output, expected);
 assert.deepStrictEqual(myArray, unchanged);
+
+//Exercicio 2.2
+const wordLengths = (words) => {
+  let result = [];
+
+  for (let index = 0; index < words.length; index += 1) {
+    const element = words[index];
+    result.push(element.length);
+  }
+
+  return result;
+};
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected1 = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output1 = wordLengths(words);
+assert.deepStrictEqual(output1, expected1);
