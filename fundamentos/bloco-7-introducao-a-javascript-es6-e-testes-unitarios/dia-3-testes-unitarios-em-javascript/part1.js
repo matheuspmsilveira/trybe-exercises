7af086e895f9ffbe53c5ff47893e4835cd2cf0d8
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-07-26 16:11:25 -0300

    Exercicio 1.5 - Compare dois objetos para verificar se são idênticos ou não

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/part1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/part1.js
@@ -69,3 +69,23 @@ assert.strictEqual(myFizzBuzz(9), 'fizz');
 assert.strictEqual(myFizzBuzz(10), 'buzz');
 assert.strictEqual(myFizzBuzz(7), 7);
 assert.strictEqual(myFizzBuzz('oi'), false);
+
+//Exercicio 5
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+assert.deepStrictEqual(obj1, obj2, 'objetos diferentes');
+assert.notDeepStrictEqual(obj1, obj3, 'objetos iguais');
+assert.notDeepStrictEqual(obj2, obj3, 'objetos iguais');
