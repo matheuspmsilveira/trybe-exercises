9c469d5ebb5b86fa01fcb5826dcd28bdb051d364
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-07-28 16:09:47 -0300

    Exercicios 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas. Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -27,3 +27,27 @@ const lotteryResult = (number, func) => {
 };
 
 console.log(lotteryResult(3, resultCheck));
+
+//Exercicio 3
+const answersComparison = (rightAnswers, studentAnswers) => {
+  if (rightAnswers === studentAnswers) {
+    return 1;
+  } else if (studentAnswers === 'N.A') {
+    return 0;
+  } else {
+    return -0.5;
+  }
+};
+
+const testScore = (rightAnswers, studentAnswers, callback) => {
+  let counter = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    counter += callback(rightAnswers[index], studentAnswers[index]);
+  }
+  return counter;
+}
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+console.log(testScore(RIGHT_ANSWERS, STUDENT_ANSWERS, answersComparison));
