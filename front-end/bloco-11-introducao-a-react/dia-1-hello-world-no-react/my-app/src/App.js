f7d54dbe747afecd28bcf1e72a08d3cbbbd8da4f
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-08-31 14:13:35 -0300

    impli: task

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/my-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/my-app/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 const compromissos = ['acordar', 'escovar os dentes', 'tomar cafe', 'estudar'];
 
-const Task = (value) => {
-  return <li>{value}</li>;
+const Task = () => {
+  return compromissos.map((compromisso) => <li>{compromisso}</li>);
 };
 
 function App() {
-  return <ul>{compromissos.map((compromisso) => Task(compromisso))}</ul>;
+  return (
+    <ul>
+      <Task />
+    </ul>
+  );
 }
 
 export default App;
